e2b47d0855c6f9a96f8356d326dffcbcc4e644ee
MarkSudnev <m.sudnev@gmail.com>
2023-04-29 00:03:18 +0200

    initial commit

---
+++ src/test/java/pl/flathumor/kubik/KubikApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.flathumor.kubik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubikApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
